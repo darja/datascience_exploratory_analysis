7b61161015b7131259dbc70a3726fb80e306b6f5
Darja Ryazhskikh <darja.ryazhskikh@gmail.com>
2017-08-12 20:26:28 +0300

    plot2

---
+++ plot2.R
@@ -0,0 +1,10 @@
+nei <- readRDS("summarySCC_PM25.rds")
+scc <- readRDS("Source_Classification_Code.rds")
+
+total <- nei %>%
+    filter(fips == 24510) %>%
+    group_by(year) %>% 
+    summarise(Emissions = sum(Emissions))
+
+plot(total, pch=20, main="Total Emissions in Baltimore City, Maryland")
+lines(total, lwd=3)
