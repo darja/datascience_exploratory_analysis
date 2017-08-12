044b06d6f5ff01c3f60e927e520d564d8319ebad
Darja Ryazhskikh <darja.ryazhskikh@gmail.com>
2017-08-12 21:18:26 +0300

    plot4

---
+++ plot4.R
@@ -0,0 +1,17 @@
+library(dplyr)
+library(ggplot2)
+
+nei <- readRDS("summarySCC_PM25.rds")
+scc <- readRDS("Source_Classification_Code.rds")
+
+coal_scc <- scc %>% 
+    filter(str_detect(EI.Sector, "Coal")) %>%
+    select(c(SCC, EI.Sector))
+
+coal_emissions <- nei %>%
+    filter(SCC %in% coal_scc$SCC) %>%
+    group_by(year) %>% 
+    summarise(Emissions = sum(Emissions))
+
+plot(coal_emissions, pch=20, main="Coal-Related Emissions")
+lines(coal_emissions, lwd=3)
