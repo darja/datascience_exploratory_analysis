53ff01981c57464eaff2d9cdae2f3d93c5b782d1
Darja Ryazhskikh <darja.ryazhskikh@gmail.com>
2017-08-12 21:28:10 +0300

    plot5

---
+++ plot5.R
@@ -0,0 +1,16 @@
+library(dplyr)
+
+nei <- readRDS("summarySCC_PM25.rds")
+scc <- readRDS("Source_Classification_Code.rds")
+
+vehicle_scc <- scc %>% 
+    filter(str_detect(EI.Sector, "Vehicle")) %>%
+    select(c(SCC, EI.Sector))
+
+baltimore_vehicle_emissions <- nei %>%
+    filter(fips == 24510 & SCC %in% vehicle_scc$SCC) %>%
+    group_by(year) %>% 
+    summarise(Emissions = sum(Emissions))
+
+plot(baltimore_vehicle_emissions, pch=20, main="Emissions From Motor Vehicle Sources in Baltimore City")
+lines(baltimore_vehicle_emissions, lwd=3)
